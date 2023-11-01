d5f26c25858821f1ed4d401d189c231b0322ee38
Emil Jørgensen <1709emil@gmail.com>
2023-11-01 13:30:47 +0100

    View template

--- WebTech_FarmShop/resources/views/frontpage.blade.php
+++ WebTech_FarmShop/resources/views/frontpage.blade.php
@@ -1,47 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>TesterSite</title>
-    <link rel="stylesheet" href="{{asset('css/stylesheet.css')}}">
-
-    <script src="------"></script>
-
-</head>
-<body>
-
-
-<header>
-    <h1 id="title">Music Library</h1>
-</header>
-<div class="flex-div">
-    <nav>
-
-        <h2>Menu Header</h2>
-        <ul>
-            <li>Menu 1</li>
-            <li>Menu 2</li>
-            <li>Menu 3</li>
-        </ul>
-    </nav>
-
-    <section id="maindiv">
-
-
-
-    </section>
-</div>
-
-
-<footer>
-    <ul>
-        <li>Contact: 23422324</li>
-
-    </ul>
-</footer>
-
-
-</body>
-
-
-</html>
+@extends('ViewTemplate')
+@section('section')
+    @parent
+    <p>Hello world</p>
+@endsection
