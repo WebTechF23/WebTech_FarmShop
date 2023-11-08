9705390beeca361fcb34c150778643614dd4c9e4
hemgaard <pehem22@student.sdu.dk>
2023-11-08 11:15:37 +0100

    Created Userpage.php

---
+++ WebTech_FarmShop/resources/views/UserPage.blade.php
@@ -0,0 +1,34 @@
+<html>
+    <head>
+
+    </head>
+    <body>
+        <nav>
+            <h3>Welcome to my website</h3>
+        <hr>
+        </nav>
+
+        <form>
+            <div class="forms-grouping">
+                <label for="email">Email: </label><br>
+                <input type="email" id="email" name="email"><br>
+
+                <label for="fname">First name: </label><br>
+                <input type="text" id="fname" name="fname"><br>
+
+                <label for="lname">Last name: </label><br>
+                <input type="text" id="lname" name="lname"><br>
+
+
+                <button type="submit"></button>
+
+            </div>
+
+        </form>
+
+<footer>
+    <hr />
+    © 2023 Group 5
+</footer>
+</body>
+</html>
