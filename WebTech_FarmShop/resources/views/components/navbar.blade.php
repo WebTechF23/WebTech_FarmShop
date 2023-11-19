e5104280079e7b22b6725a6c950b72396a58cd7b
Martin Dam <marda22@student.sdu.dk@>
2023-11-19 23:45:39 +0100

    Made progress on the navbar
    Made progress on the navbar

---
+++ WebTech_FarmShop/resources/views/components/navbar.blade.php
@@ -0,0 +1,13 @@
+<div class="navbar">
+    <img class="logo" src="https://images.pexels.com/photos/458991/pexels-photo-458991.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="logo">
+    <nav class="bar">
+        <ul class="nav_links">
+            <li><a href="/">Home</a></li>
+            <li><a href="/login">Login</a></li>
+            <li><a href="/buy">Buy</a></li>
+            <li><a href="/basket">Basket</a></li>
+        </ul>
+    </nav>
+</div>
+
+
