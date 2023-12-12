df1713811f2874ec1d00df62a5770d1d32e9d12c
hemgaard <pehem22@student.sdu.dk>
2023-12-12 18:57:44 +0100

    out comment

--- WebTech_FarmShop/resources/views/userpage.blade.php
+++ WebTech_FarmShop/resources/views/userpage.blade.php
@@ -14,9 +14,9 @@
 @section('main')
     <div>
         <h2>User Information</h2>
-        <p>User Name: {{$users->name}}</p>
+        {{--<p>User Name: {{$users->name}}</p>
         <p>Email: {{$users->email}}</p>
-        <p>Phone nr: {{$users->phoneNumber}}</p>
+        <p>Phone nr: {{$users->phoneNumber}}</p>--}}
         <h2>Order History</h2>
         <table >
             <thead>
