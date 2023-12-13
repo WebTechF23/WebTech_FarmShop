c99a3006f49927ddcea96913ce101bf0a1a7f547
Mads Wrang Sigsgaard <mads54325@gmail.com>
2023-12-13 16:01:19 +0100

    Created ProductTest.php to test if seeder does the job properly

---
+++ WebTech_FarmShop/tests/Unit/ProductTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+
+class ProductTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function it_seeds_users_stocks_pictures_and_products()
+    {
+        $this->seed();
+
+        // Assert that two users are created
+        $this->assertCount(2, \App\Models\User::all());
+
+        // Assert that six stocks are created
+        $this->assertCount(6, \App\Models\Stock::all());
+
+        // Assert that six pictures are created
+        $this->assertCount(6, \App\Models\Picture::all());
+
+        // Assert that six products are created
+        $this->assertCount(6, \App\Models\Product::all());
+
+        // Add more assertions if needed to check specific data in the created records
+    }
+}
