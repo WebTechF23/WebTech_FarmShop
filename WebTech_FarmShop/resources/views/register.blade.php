5fcca05ada99e9902c739f8ae836b46c52dd343b
hemgaard <pehem22@student.sdu.dk>
2023-12-13 18:11:21 +0100

    css register page

--- WebTech_FarmShop/resources/views/register.blade.php
+++ WebTech_FarmShop/resources/views/register.blade.php
@@ -15,19 +15,19 @@
 @section('main')
     <form action="{{url('RegisterController/register')}}" method="POST">
         @csrf
-        <div class="forms-grouping">
+        <div class="user_container">
 
             <label for="name">Name: </label><br>
-            <input type="text" id="name" name="name"><br>
+            <input type="text" id="name" name="name" class="btn"><br>
 
             <label for="password">Password: </label><br>
-            <input type="password" id="password" name="password"><br>
+            <input type="password" id="password" name="password" class="btn"><br>
 
             <label for="email">Email: </label><br>
-            <input type="email" id="email" name="email"><br>
+            <input type="email" id="email" name="email" class="btn"><br>
 
             <label for="phoneNum">Phone number: </label><br>
-            <input type="tel" id="phoneNum" name="phoneNumber"><br>
+            <input type="tel" id="phoneNum" name="phoneNumber" class="btn"><br>
 
             <button class="submitBtn" type="submit">Submit</button>
 
