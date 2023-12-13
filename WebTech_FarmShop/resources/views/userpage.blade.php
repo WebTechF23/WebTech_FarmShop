036f5e31c1dc09461027c86cbfb4d92bce781f98
Martin Dam <marda22@student.sdu.dk@>
2023-12-13 16:22:23 +0100

    fucking lort changes pete is a reet

--- WebTech_FarmShop/resources/views/userpage.blade.php
+++ WebTech_FarmShop/resources/views/userpage.blade.php
@@ -1,45 +1,42 @@
-@extends('ViewTemplate')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('title')
-    <title>User page</title>
-@endsection
-
-
-@section('header')
-    <h1>User page</h1>
-@endsection
+<head>
 
 
+    <title></title>
+</head>
+<body>
+<div>
+    <ul>
+        <li>My Details</li>
+        <li>My Orders</li>
+        <li></li>
+    </ul>
+</div>
 
-@section('main')
-    <div>
-        <ul>
-            <li>My Details</li>
-            <li>My Orders</li>
-            <li></li>
-        </ul>
-    </div>
+<div class="contentContainer">
 
-    <div class="contentContainer">
-
-        <textarea disabled></textarea>
-        <label for="fnameID">First name</label>
+    <textarea disabled></textarea>
+    <label for="fnameID">First name</label>
         <input type="text" id="fnameID">
 
-        <label for="lnameID">Last name</label>
-        <input type="text" id="lnameID">
+    <label for="lnameID">Last name</label>
+    <input type="text" id="lnameID">
 
-        <label for="emailID">Email </label>
-        <input type="email" id="emailID">
+    <label for="emailID">Email </label>
+    <input type="email" id="emailID">
 
-        <p>{{session()->get('phoneNumber')}}</p>
-{{--        {{session()->}}--}}
-        @if(session()->has('name'))
-            <p>Value from session: {{ session('name') }}</p>
-        @else
-            <p>No value found in the session for the specified key.</p>
-        @endif
+</div>
 
-    </div>
-@endsection
 
+
+</body>
+
+
+
+
+
+
+
+</html>
