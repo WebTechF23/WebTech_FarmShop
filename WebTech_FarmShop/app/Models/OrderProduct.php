8e6e0279ad307a20159f7829fc47973bc1ef5611
MacBech <115424555+MacBech@users.noreply.github.com>
2023-12-22 11:08:56 +0100

    Dumme model

---
+++ WebTech_FarmShop/app/Models/OrderProduct.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OrderProduct extends Model
+{
+    use HasFactory;
+
+    //Is this right?
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+
+}
